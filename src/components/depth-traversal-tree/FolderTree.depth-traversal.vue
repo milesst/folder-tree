fc2246bfd805fec7b6144def1abc9d11471226ae
alla <alla1rafikova@gmail.com>
2024-11-23 16:14:18 +0300

    depth-traversal tree impl

---
+++ src/components/depth-traversal-tree/FolderTree.depth-traversal.vue
@@ -0,0 +1,101 @@
+<template lang="">
+  <div 
+    class="folder-tree-wrap">
+    <FolderTreeNode
+      v-for="folderNode in visibleNodes"
+      :key="folderNode.folder.id"
+
+      :node="folderNode"
+      :selected="folderNode.folder === selectedFolder"
+      @select="handleSelect(folderNode)"/>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { Folder } from '../../types/Folder';
+import { FolderNode } from '../../types/FolderNode';
+import FolderTreeNode from './FolderTreeNode.vue';
+
+const emit = defineEmits(['select'])
+
+const props = defineProps<{
+    data: Folder[],
+    }>()
+const selectedFolder = defineModel()
+
+
+const handleSelect = (folderNode: FolderNode) => {
+  if (folderNode.folder === selectedFolder.value)
+    selectedFolder.value = null
+  else
+    selectedFolder.value = folderNode.folder
+  emit('select', folderNode?.folder?.id)
+}
+    
+const updateFolderTreeNodes = (folders: Folder[]) => {
+  let depth = 0
+  const rootNode: FolderNode = {
+    depth: depth++,
+    childrenNodes: folders,
+    visited: false,
+    open: true
+  }
+  const queue = [rootNode]
+  const stack: FolderNode[] = []
+  const getChildrenNodes = (node?: FolderNode): (FolderNode | Folder)[] => {
+    return node?.childrenNodes?.slice().reverse() || []
+  } 
+  while (queue.length > 0) {
+    const currentNode = queue.pop()
+    if (currentNode && currentNode.folder) {
+      stack.push(currentNode)
+      currentNode.visited = true
+    }
+    if (currentNode?.open)
+      for (let childNode of getChildrenNodes(currentNode)) {
+          let mappedNode: FolderNode
+          if (!(childNode as FolderNode).depth)
+              mappedNode = {
+                  folder: childNode,
+                  parentNode: currentNode,
+                  depth: (currentNode?.depth || 0) + 1,
+                  childrenNodes: (childNode as Folder)?.children,
+                  open: true
+              } as FolderNode
+          else
+              mappedNode = childNode as FolderNode
+          if (!mappedNode.visited) {
+              mappedNode.visited = true
+              queue.push(mappedNode)
+          }
+      }
+  }
+  return stack
+}
+
+const folderTreeNodes = ref(updateFolderTreeNodes(props.data))
+
+const visibleNodes = computed(() => {
+  const resultArray = [] as FolderNode[]
+  let i = 0
+  while (i < folderTreeNodes.value.length) {
+    resultArray.push(folderTreeNodes.value[i])
+    if (folderTreeNodes.value[i].open) {
+      i++
+      continue;
+    }
+    const parentNodeDepth = folderTreeNodes.value[i].depth
+    i++
+    while (i < folderTreeNodes.value.length && folderTreeNodes.value[i].depth > parentNodeDepth) {
+      i++
+    }
+  }
+  return resultArray
+})
+
+</script>
+<style scoped>
+.folder-tree-wrap {
+  padding: 0.5rem;
+}
+</style>
---
+++ src/components/depth-traversal-tree/FolderTreeNode.vue
@@ -0,0 +1,71 @@
+<template lang="">
+    <div
+        class="folder-node"
+        :style="{ 'margin-left': (node.depth*2.5)+'rem' }">
+        <div 
+            class="folder-icon" 
+            @click="() => { node.open = !node.open }" >
+            {{ node.open ? '-' : '+' }}
+        </div>
+        <div 
+            @click="emit('select')" 
+            class="folder-name" 
+            :class="selected ? 'selected' : ''">
+            📁 {{ node.folder.name }}
+        </div>
+        <div 
+            v-if="selected" 
+            class="folder-check">
+            ✓
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { FolderNode } from '../../types/FolderNode';
+
+const emit = defineEmits(['select'])
+const props = defineProps<{
+    node: FolderNode,
+    selected: boolean
+}>()
+</script>
+<style>
+.folder-node {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+}
+.selected {
+    color: hsl(202, 100%, 62%);
+}
+.folder-name {
+    padding: 0.5rem 1rem 0.5rem;
+    border-radius: 10px;
+}
+.folder-name:hover {
+    color: hsl(202, 100%, 62%);
+}
+.folder-icon {
+    width: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    margin-right: 0.5rem;
+    user-select: none;
+}
+.folder-node:not(:last-child) {
+    margin-bottom: 1rem;
+}
+.folder-check {
+    font-size: 0.7rem;
+    height: 1rem;
+    width: 1rem;
+    color: white;
+    background-color: hsl(202, 100%, 62%);
+    border-radius: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
