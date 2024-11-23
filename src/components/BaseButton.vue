448f78fc8c484141350a4c76c5618baddb192396
alla <alla1rafikova@gmail.com>
2024-11-23 16:14:47 +0300

    linting

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -7,15 +7,15 @@ const props = defineProps<{
 }>()
 </script>
 <style scoped>
-    button {
-        background: hsl(202, 100%, 62%);
-        border-radius: 10px;
-        border: none;
-        color: white;
-        padding: 1rem 1.5rem;
-    }
-    button:hover {
-        background: hsl(202, 100%, 42%);
-        cursor: pointer;
-    }
+button {
+    background: hsl(202, 100%, 62%);
+    border-radius: 10px;
+    border: none;
+    color: white;
+    padding: 1rem 1.5rem;
+}
+button:hover {
+    background: hsl(202, 100%, 42%);
+    cursor: pointer;
+}
 </style>
